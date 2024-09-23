276288ec5ecdc97e42be45debf2381ec9b93d53a
achalaidiuk <sasha.treyser90@gmail.com>
2024-09-23 13:34:23 +0300

    fixed review changes

--- taxi/forms.py
+++ taxi/forms.py
@@ -5,21 +5,31 @@ from django.core.validators import RegexValidator, MaxLengthValidator
 from taxi.models import Driver, Car
 
 
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+
+from taxi.models import Driver, Car
+
 class DriverCreationForm(UserCreationForm):
     license_number = forms.CharField(
+        max_length=8,
         validators=[
             RegexValidator(
                 regex=r"^[A-Z]{3}\d{5}$",
-                message="License number must consist of"
-                        " 3 uppercase letters followed by 5 digits."
+                message="License number must consist of 3 uppercase letters followed by 5 digits."
             ),
-            MaxLengthValidator(
-                8, "License number must be exactly 8 characters."
-            )
         ]
     )
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise ValidationError("License number must be exactly 8 characters.")
+        return license_number
 
     class Meta:
         model = Driver
@@ -29,9 +39,25 @@ class DriverCreationForm(UserCreationForm):
 
 
 class DriverLicenseUpdateForm(forms.ModelForm):
+    license_number = forms.CharField(
+        max_length=8,
+        validators=[
+            RegexValidator(
+                regex=r"^[A-Z]{3}\d{5}$",
+                message="License number must consist of 3 uppercase letters followed by 5 digits."
+            ),
+        ]
+    )
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise ValidationError("License number must be exactly 8 characters.")
+        return license_number
+
     class Meta:
         model = Driver
-        fields = ("license_number", )
+        fields = ("license_number",)
 
 
 class CarForm(forms.ModelForm):
